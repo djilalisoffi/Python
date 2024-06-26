3351a0844062fa2d4cd5e82988e9ade98617f114
djilali soffi <djilalisoffi2023@gmail.com.com>
2024-06-26 20:09:40 +0100

    second commit

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("Hello World !!!")
+x = 5
+y = int(input("Enter a number between 0 and 10\n"))
+if y == x:
+    print("You won. Congratulations!!!")
+    print("You picked the right number.")
+else:
+    print("Wrong choice. Try again.")
